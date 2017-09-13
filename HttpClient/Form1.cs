c6297d043098594169cc3dc57cd4854a73e5154a
zhaoxf101 <zhaoxf101@qq.com>
2017-09-13 11:43:07 +0800

    1

--- HttpClient/Form1.cs
+++ HttpClient/Form1.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Management;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,25 +22,137 @@ namespace HttpClient
 
             ConfigManager.Refresh();
             textBox1.Text = ConfigManager.Host;
+
+            Load += Form1_Load;
+          
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Test();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            ConfigManager.Host = textBox1.Text;
-            ConfigManager.Save();
+            //ConfigManager.Host = textBox1.Text;
+            //ConfigManager.Save();
 
 
+            //try
+            //{
+            //    Get($"http://{textBox1.Text}/");
+            //    Log($"Get {textBox1.Text} success.");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Log($"Exception: {ex.Message}");
+            //}
+
+        }
+
+        void Test()
+        {
+            Log("win32_processor");
             try
             {
-                Get($"http://{textBox1.Text}/");
-                Log($"Get {textBox1.Text} success.");
+                System.Management.ManagementClass mc = new ManagementClass("win32_processor");
+                Log("new ManagementClass");
+                ManagementObjectCollection moc = mc.GetInstances();
+                Log("mc.GetInstances");
+
+                foreach (ManagementObject mo in moc)
+                {
+                    Log(mo["processorid"].ToString());
+                }
+
+
             }
             catch (Exception ex)
             {
-                Log($"Exception: {ex.Message}");
+                Log($"ex: {ex.Message}");
             }
 
+            Log("SerialNumber");
+            System.Management.ManagementClass mcBoard = new System.Management.ManagementClass("Win32_BaseBoard");
+            Log("new ManagementClass");
+
+            ManagementObjectCollection mocBoard = mcBoard.GetInstances();
+            Log("mc.GetInstances");
+
+            foreach (ManagementObject mx in mocBoard)
+            {
+                Log(mx.Properties["SerialNumber"].Value.ToString());
+            }
+
+            Log("Win32_DiskDrive");
+            try
+            {
+                System.Management.ManagementClass mcDisk = new System.Management.ManagementClass("Win32_DiskDrive");
+                Log("new ManagementClass");
+
+                ManagementObjectCollection mocDisk = mcDisk.GetInstances();
+                Log("mc.GetInstances");
+
+                foreach (ManagementObject mo in mocDisk)
+                {
+                    Log(mo.Properties["Model"].Value.ToString());
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Log($"ex: {ex.Message}");
+
+            }
+            Log("Win32_NetworkAdapterConfiguration");
+
+            try
+            {
+                System.Management.ManagementClass mcNetwork = new System.Management.ManagementClass("Win32_NetworkAdapterConfiguration");
+                Log("new ManagementClass");
+
+                ManagementObjectCollection mocNetwork = mcNetwork.GetInstances();
+                Log("mc.GetInstances");
+
+                foreach (ManagementObject mo in mocNetwork)
+                {
+
+                    if ((bool)mo["IPEnabled"] == true)
+                        Log(mo["MacAddress"].ToString());
+
+                    mo.Dispose();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Log($"ex: {ex.Message}");
+            }
+
+            Log("Win32_OperatingSystem");
+            try
+            {
+                System.Management.ManagementClass mcMemory = new System.Management.ManagementClass("Win32_OperatingSystem");
+                Log("new ManagementClass");
+
+                ManagementObjectCollection mocMemory = mcMemory.GetInstances();
+                Log("mc.GetInstances");
+
+                foreach (ManagementObject mo in mocMemory)
+                {
+                    if (mo.Properties["TotalVisibleMemorySize"].Value != null)
+                    {
+                        Log(mo.Properties["TotalVisibleMemorySize"].Value.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log($"ex: {ex.Message}");
+            }
         }
+
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -75,7 +188,7 @@ namespace HttpClient
         {
             textBox2.BeginInvoke(new Action(() =>
             {
-                textBox2.AppendText(DateTime.Now.ToString("[yyyy-MM-dd HH:mm:ss.fff] ") + message + Environment.NewLine);
+                textBox2.AppendText(/*DateTime.Now.ToString("[yyyy-MM-dd HH:mm:ss.fff] ") + */message + Environment.NewLine);
                 textBox2.ScrollToCaret();
             }));
         }
